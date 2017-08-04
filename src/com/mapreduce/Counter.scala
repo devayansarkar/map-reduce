60cf3880a48882de5937ff6005a7ad4cd37c6404
Devayan Sarkar <devayansarkar@gmail.com>
2017-08-05 00:37:51 +0200

    adding classes

---
+++ src/com/mapreduce/Counter.scala
@@ -0,0 +1,5 @@
+package com.mapreduce
+
+object Counter extends App{
+  val count = new WordCount
+  }
---
+++ src/com/mapreduce/WordCount.scala
@@ -0,0 +1,7 @@
+package com.mapreduce
+
+import org.apache.hadoop.mapreduce.Mapper
+
+class WordCount {
+ 
+}
