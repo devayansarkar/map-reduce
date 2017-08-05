6bdea8b1c18bd6d01adf5bf23fe55a68638df1a8
Devayan Sarkar <devayansarkar@gmail.com>
2017-08-05 23:55:47 +0200

    adding simple reducer logic

--- src/com/mapreduce/ReduceData.scala
+++ src/com/mapreduce/ReduceData.scala
@@ -6,4 +6,13 @@ import org.apache.hadoop.io.Text
 
 class ReduceData extends Reducer[Text,IntWritable,Text,IntWritable]{
   
+  def reduce(key:Text, values:Iterable[IntWritable],context:Context):Unit={
+    var sum = 0 
+    for(value <- values){
+      sum+=value.get()
+    }
+    context.write(key,new IntWritable(sum))
+  
+  }
+  
 }
