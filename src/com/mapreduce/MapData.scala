9eb3213384f224d6e63aad23eda13cddaa03a89c
Devayan Sarkar <devayansarkar@gmail.com>
2017-08-05 23:44:37 +0200

    adding simple mapper logic

--- src/com/mapreduce/MapData.scala
+++ src/com/mapreduce/MapData.scala
@@ -3,7 +3,19 @@ import org.apache.hadoop.mapreduce.Mapper
 import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.io.IntWritable
 import org.apache.hadoop.io.Text
+import java.io.IOException
+import java.util.StringTokenizer
 
 class MapData extends Mapper[LongWritable, Text, Text, IntWritable] {
   
+  @throws(classOf[IOException])
+  def map(key:LongWritable ,value:Text,context:Context):Unit={
+    var line = value.toString()
+    var tokenizer = new StringTokenizer(line)
+    
+    while(tokenizer.hasMoreTokens()){
+      value.set(tokenizer.nextToken())
+      context.write(value, new IntWritable)
+    }
+  }
 }
--- src/com/mapreduce/WordCount.scala
+++ src/com/mapreduce/WordCount.scala
@@ -3,5 +3,9 @@ package com.mapreduce
 import org.apache.hadoop.mapreduce.Mapper
 
 class WordCount {
- 
+ def countWords():String={
+    val mapper = new MapData
+    val reducer = new ReduceData
+    "fancy String"
+ }
 }
